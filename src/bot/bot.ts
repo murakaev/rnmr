5d7aa45edea7da7d08ba5293a7e5dc99dc0b8d54
Timur Murakaev <murakaev.tt@icloud.com>
2026-07-16 11:27:27 +0300

    fix(config): terminate app on startup if BOT_TOKEN is not set

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -4,8 +4,8 @@ import { Checker } from '../core/checker'
 import { registerCheckCommand } from './commands/check'
 import { registerStartCommand } from './commands/start'
 
-export function createBot(checker: Checker): Telegraf {
-  const bot = new Telegraf(process.env.BOT_TOKEN!)
+export function createBot(checker: Checker, token: string): Telegraf {
+  const bot = new Telegraf(token)
 
   registerStartCommand(bot)
   registerCheckCommand(bot, checker)
--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,15 @@ import { Checker } from './core/checker'
 import { GitHubProvider } from './providers/github'
 import { HTTPClient } from './services/http'
 
+if (!process.env.BOT_TOKEN) {
+  console.error('BOT_TOKEN is not set')
+  process.exit(1)
+}
+
 const http = new HTTPClient()
 
 const checker = new Checker([new GitHubProvider(http)])
 
-const bot = createBot(checker)
+const bot = createBot(checker, process.env.BOT_TOKEN!)
 
 bot.launch()
