f544e1bac4569966cd4f602588008e050fdd80ac
Timur Murakaev <murakaev.tt@icloud.com>
2026-07-21 15:08:27 +0300

    feat(checker): implement caching mechanism for provider checks

--- src/core/checker.ts
+++ src/core/checker.ts
@@ -2,10 +2,31 @@ import type { Provider } from '../models/provider'
 import { ProviderResult } from '../models/result'
 
 export class Checker {
-  constructor(private providers: Provider[]) {}
+  private cache = new Map<
+    string,
+    { result: ProviderResult[]; expires: number }
+  >()
+
+  constructor(
+    private providers: Provider[],
+    private CACHE_TTL_MS: number = 10 * 60 * 1000 // 10 min
+  ) {}
 
   async check(name: string): Promise<ProviderResult[]> {
-    return Promise.all(
+    const key = name.toLowerCase()
+    const now = Date.now()
+    const cached = this.cache.get(key)
+
+    if (cached && now < cached.expires) {
+      console.log('Used cache!!!')
+      return cached.result
+    }
+
+    if (cached && now > cached.expires) {
+      this.cache.delete(key)
+    }
+
+    const result: ProviderResult[] = await Promise.all(
       this.providers.map(async (provider) => {
         try {
           return await provider.check(name)
@@ -19,5 +40,9 @@ export class Checker {
         }
       })
     )
+
+    this.cache.set(key, { result, expires: now + this.CACHE_TTL_MS })
+
+    return result
   }
 }
